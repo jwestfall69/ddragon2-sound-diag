b1ca2aca1c5e0e85ed4618f3682ed589683a877c
jwestfall <jwestfall@surrealistic.net>
2022-04-24 12:35:45 -0700

    Adjust how we handle test results

--- ddragon2_sound_diag.asm
+++ ddragon2_sound_diag.asm
@@ -13,27 +13,19 @@ _start:
 		ld	a,(MMIO_MAIN_CPU_LATCH)
 
 		PSUB	ram_oe_test
-		jr	z, .test_passed_ram_oe
-		jp	EA_RAM_DEAD_OUTPUT
-	.test_passed_ram_oe:
+		jp	nz, EA_RAM_DEAD_OUTPUT
 
 		PSUB	ram_we_test
-		jr	z, .test_passed_ram_we
-		jp	EA_RAM_UNWRITABLE
-	.test_passed_ram_we:
+		jp	nz, EA_RAM_UNWRITABLE
 
 		PSUB	ram_data_tests
-		jr	z, .test_passed_ram_data
-		jp	EA_RAM_DATA
-	.test_passed_ram_data:
+		jp	nz, EA_RAM_DATA
 
 		PSUB	ram_address_test
-		jr	z, .test_passed_ram_address
-		jp	EA_RAM_ADDRESS
-	.test_passed_ram_address:
+		jp	nz, EA_RAM_ADDRESS
+
+		; ram seems good, init stack
+		ld	sp, RAM_START + RAM_SIZE - 2
 
 
 		jp	EA_ALL_PASSED
-test_psub:
-	ld	a,$44
-	PSUB_RETURN
